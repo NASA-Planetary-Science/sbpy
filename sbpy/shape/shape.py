ebb12c0c992f93f0a8c7bbc349553e7caf8ce53e
Michael Mommert <michael.mommert@nau.edu>
2017-06-28 13:49:17 -0700

    lightcurve class added

--- sbpy/shape/shape.py
+++ sbpy/shape/shape.py
@@ -7,7 +7,7 @@ SBPy Shape Module
 created on June 26, 2017
 """
 
-__all__ = ['ModelClass', 'ROLO', 'Kaasalainen']
+__all__ = ['ModelClass', 'ROLO', 'Kaasalainen', 'Lightcurve']
 
 class ModelClass():
 
@@ -20,13 +20,13 @@ class ModelClass():
     def get_facet(self, facetid):
         """Retrieve information for one specific surface facet"""
 
-    def iof(self, facetid, ephem):
+    def iof(self, facetid, eph):
         """Derive I/F for one specific surface facet"""
 
-    def integrated_flux(self, ephem):
+    def integrated_flux(self, eph):
         """Derive surface-integrated flux"""
 
-    def lightcurve(self, ephem, time):
+    def lightcurve(self, eph, time):
         """Derive lightcurve"""
         
         
@@ -44,11 +44,40 @@ class Kaasalainen(ModelClass):
         self.properties = None
         # setup model properties
 
-    def convexinv(self, ephem):
+    def convexinv(self, eph):
         """Obtain shape model through lightcurve inversion, optimizing all
         parameters and uses spherical harmonics functions for shape
         representation"""
 
-    def conjgradinv(self, ephem):
+    def conjgradinv(self, eph):
         """Obtain shape model through lightcurve inversion, optimizing only
         shape and uses directly facet areas as parameters"""
+
+
+class Lightcurve():
+
+    def __init__(self, eph):
+        self.eph = eph
+        self.fouriercoeff = None
+        self.period = None
+        self.pole = (0, 90) # ecliptic coordinates
+        
+    def axis_ratio(self):
+        """Derive axis ratio from lightcurve amplitude"""
+        
+    def derive_period(self, method='lomb-scargle'):
+        """Derive lightcurve period using different methods"""
+
+    def fit_fourier(self, order):
+        """Fit Fourier coefficients to lightcurve"""
+
+    def fit_pole(self):
+        """Fit pole orientation"""
+
+    def fit(self):
+        """Fit period, pole orientation, and Fourier coefficients at the same time"""
+        
+    def simulate(self):
+        """Simulate a lightcurve from period, Fourier coefficients, pole orientation"""
+
+        
