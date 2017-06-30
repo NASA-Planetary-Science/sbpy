693714805152cadc3b1251fa369e23dc9b51cc63
Michael S. P. Kelley <msk@astro.umd.edu>
2017-06-30 12:44:43 -0400

    Parse comet and asteroid target names.
    The testing is strict: letter case must be correct and there cannot be
    any extra characters (except leading a trailing spaces are stripped
    before the test).
    
    Both functions raise an exception if the target name does not match
    the pattern.

--- sbpy/data/data.py
+++ sbpy/data/data.py
@@ -12,8 +12,10 @@ from astropy.time import Time
 import astropy.units as u
 import callhorizons
 
-__all__ = ['Orbit', 'Ephem', 'Phys', 'Misc', 'DataClass']
+__all__ = ['Orbit', 'Ephem', 'Phys', 'Misc', 'DataClass', 'parse_comet', 'parse_asteroid']
 
+class TargetNameParseError(Exception):
+    pass
 
 class DataClass():
 
@@ -812,3 +814,152 @@ class Misc():
     def to_packed(targetid):
         """ Convert unpacked identifier to packed designation/number"""
         
+def parse_comet(s):
+    """Parse a string as if it were a comet name.
+
+    Only considers IAU-formatted permanent and new-style designations.
+    Note that letter case is important.
+
+    Parameters
+    ----------
+    s : string
+      The string to parse.
+
+    Returns
+    -------
+    des : string
+      The designation of the comet, e.g., '1P', or 'C/1995 O1'.
+    name : string
+      The name of the comet, if provided in `s`, e.g., 'Halley', or
+      'Hale-Bopp'.
+
+    Raises
+    ------
+    TargetNameParseError : Exception
+      If the string does not appear to be a comet name.
+
+    Examples
+    --------
+    The following table shows the result of the designation parsing:
+
+      +-------------------------------+-------------+-------------------------+
+      |targetname                     |des          |name                     |
+      +===============================+=============+=========================+
+      |1P/Halley                      |1P           |Halley                   |
+      +-------------------------------+-------------+-------------------------+
+      |3D/Biela                       |3D           |Biela                    |
+      +-------------------------------+-------------+-------------------------+
+      |9P/Tempel 1                    |9P           |Tempel 1                 |
+      +-------------------------------+-------------+-------------------------+
+      |73P/Schwassmann-Wachmann 3 C   |73P          |Schwassmann-Wachmann 3 C |
+      +-------------------------------+-------------+-------------------------+
+      |73P-C/Schwassmann-Wachmann 3 C |73P-C        |Schwassmann-Wachmann 3 C |
+      +-------------------------------+-------------+-------------------------+
+      |73P-BB                         |73P-BB       |                         |
+      +-------------------------------+-------------+-------------------------+
+      |322P                           |322P         |                         |
+      +-------------------------------+-------------+-------------------------+
+      |X/1106 C1                      |X/1106 C1    |                         |
+      +-------------------------------+-------------+-------------------------+
+      |P/1994 N2 (McNaught-Hartley)   |P/1994 N2    |McNaught-Hartley         |
+      +-------------------------------+-------------+-------------------------+
+      |P/2001 YX127 (LINEAR)          |P/2001 YX127 |LINEAR                   |
+      +-------------------------------+-------------+-------------------------+
+      |C/-146 P1                      |C/-146 P1    |                         |
+      +-------------------------------+-------------+-------------------------+
+      |C/2001 A2-A (LINEAR)           |C/2001 A2-A  |LINEAR                   |
+      +-------------------------------+-------------+-------------------------+
+      |C/2013 US10                    |C/2013 US10  |                         |
+      +-------------------------------+-------------+-------------------------+
+      |C/2015 V2 (Johnson)            |C/2015 V2    |Johnson                  |
+      +-------------------------------+-------------+-------------------------+
+
+    """
+
+    import re
+
+    pat = ('((^([1-9][0-9]*[PD](-[A-Z]{1,2})?)(/(.+))?$)'
+           '|(^([CPX]/-?[0-9]{1,4} [A-Z]{1,2}[1-9][0-9]{0,2}(-[A-Z]{1,2})?)\s*(\((.+)\))?$))')
+
+    m = re.findall(pat, s.strip())
+    if len(m) > 0:
+        m = m[0]
+        if len(m[2]) > 0:  # Permament
+            return m[2], m[5]
+        elif len(m[7]) > 0:
+            return m[7], m[10]
+
+    raise TargetNameParseError('{} does not appear to be a comet name'.format(s))
+
+def parse_asteroid(s):
+    """Parse a string as if it were an asteroid name.
+
+    Only considers IAU-formatted permanent and new-style designations.
+    Note that letter case is important.
+
+    Parameters
+    ----------
+    s : string
+      The string to parse.
+
+    Returns
+    -------
+    des : string
+      The designation of the asteroid, e.g., '1', or '2014 MU69'.
+    name : string
+      The name of the asteroid, if provided in `s`, e.g., 'Ceres'.
+
+    Examples
+    --------
+    The following table shows the result of the parsing:
+
+      +-----------------+-----------+----------+
+      |targetname       |des        |name      |
+      +=================+===========+==========+
+      |1                |1          |          |
+      +-----------------+-----------+----------+
+      |(2001) Einstein  |2001       |Einstein  |
+      +-----------------+-----------+----------+
+      |2001 AT1         |2001 AT1   |          |
+      +-----------------+-----------+----------+
+      |(1714) Sy        |1714       |Sy        |
+      +-----------------+-----------+----------+
+      |1714 SY          |1714 SY    |          |  # compare with previous
+      +-----------------+-----------+----------+
+      |2014 MU69        |2014 MU69  |          |
+      +-----------------+-----------+----------+
+      |2017 AA          |2017 AA    |          |
+      +-----------------+-----------+----------+
+      |(20231) 1997 YK  |20231      |1997 YK   |
+      +-----------------+-----------+----------+
+      |2040 P-L         |2040 P-L   |          |
+      +-----------------+-----------+----------+
+      |3138 T-1         |3138 T-1   |          |
+      +-----------------+-----------+----------+
+      |1010 T-2         |1010 T-2   |          |
+      +-----------------+-----------+----------+
+      |4101 T-3         |4101 T-3   |          |
+      +-----------------+-----------+----------+
+
+    """
+
+    import re
+
+    # Provisional, permanent, then survey-specific
+    pat = ('((^([1-9]|A)[0-9]{3,3}( [A-Z]{2,2}([1-9][0-9]{0,2})?)$)'
+           '|(^\(([1-9][0-9]*)\) (.+)?$)|(^[1-9][0-9]*$)'
+           '|(^[1-9][0-9]* ((P-L)|(T-[123]))$))')
+
+    m = re.findall(pat, s.strip())
+    if len(m) > 0:
+        m = m[0]
+        if len(m[1]) > 0:    # Provisional
+            return m[1], ''
+        elif len(m[6]) > 0:  # Permanent with name
+            return m[6], m[7]
+        elif len(m[8]) > 0:  # Permenent without name
+            return m[8], ''
+        elif len(m[9]) > 0:  # Survey-specific
+            return m[9], ''
+
+    raise TargetNameParseError('{} does not appear to be an asteroid name'.format(s))
