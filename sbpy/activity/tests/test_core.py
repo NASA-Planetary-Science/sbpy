fec8f28fe8c503d220260bd9baed2f404d6b557d
Michael S. P. Kelley <msk@astro.umd.edu>
2017-11-11 11:12:31 -0500

    New TestAnnularAperture and TestGaussianAperture.

--- sbpy/activity/tests/test_core.py
+++ sbpy/activity/tests/test_core.py
@@ -10,10 +10,22 @@ class TestCircularAperture:
         aper = CircularAperture(r)
         assert aper.coma_equivalent_radius() == 1 * u.arcsec
 
+class TestAnnularAperture:
+    def test_coma_equivalent_radius(self):
+        shape = [1, 2] * u.arcsec
+        aper = AnnularAperture(shape)
+        assert aper.coma_equivalent_radius() == 1 * u.arcsec
+
 class TestRectangularAperture:
     def test_coma_equivalent_radius(self):
         shape = (0.8, 2) * u.arcsec
         aper = RectangularAperture(shape)
         r = aper.coma_equivalent_radius()
         assert np.isclose(r.value, 0.66776816346357259)
+
+class TestGaussianAperture:
+    def test_coma_equivalent_radius(self):
+        sigma = 1 * u.arcsec
+        aper = GaussianAperture(sigma)
+        assert aper.coma_equivalent_radius() == np.sqrt(np.pi / 2) * u.arcsec
 
