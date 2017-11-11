b5e5ab6fa273a2e5dc0019121fc8a23f98eddea3
Michael S. P. Kelley <msk@astro.umd.edu>
2017-11-11 14:25:09 -0500

    Test rho_as_... functions.

--- sbpy/activity/tests/test_core.py
+++ sbpy/activity/tests/test_core.py
@@ -4,6 +4,22 @@ import numpy as np
 import astropy.units as u
 from ..core import *
 
+def test_rho_as_angle():
+    # arctan(100 km, 1 au) * 206264.806 = 0.13787950659645942
+    rho = rho_as_angle(100 * u.km, {'delta': 1 * u.au})
+    assert np.isclose(rho.to(u.arcsec).value, 0.13787950659645942)
+
+def test_rho_as_distance():
+    # 1 au * tan(1") = 725.2709438078363
+    rho = rho_as_distance(1 * u.arcsec, {'delta': 1 * u.au})
+    assert np.isclose(rho.to(u.km).value, 725.2709438078363)
+
+def test_rho_roundtrip():
+    a = 10 * u.arcsec
+    eph = {'delta': 1 * u.au}
+    b = rho_as_angle(rho_as_distance(a, eph), eph)
+    assert np.isclose(a.value, b.to(u.arcsec).value)
+    
 class TestCircularAperture:
     def test_coma_equivalent_radius(self):
         r = 1 * u.arcsec
