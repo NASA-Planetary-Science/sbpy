38893dc61f54834e4b9b477085a61bc0f3c49605
Michael S. P. Kelley <msk@astro.umd.edu>
2017-11-15 07:28:27 -0500

    Add Efrho tests.

--- sbpy/activity/tests/test_dust.py
+++ sbpy/activity/tests/test_dust.py
@@ -56,3 +56,45 @@ class TestAfrho:
         afrho = Afrho(10 * u.cm).to_phase(15 * u.deg, 0 * u.deg)
         assert np.isclose(afrho.cm, 5.8720)
     
+class TestEfrho:
+    def test_init(self):
+        efrho = Efrho(1000 * u.cm)
+        assert efrho.value == 1000
+        assert efrho.unit == u.cm
+
+    def test_scaler_ops(self):
+        efrho = Efrho(1000 * u.cm)
+        efrho = efrho / 2
+        assert efrho == 500 * u.cm
+
+    def test_quantity_ops(self):
+        efrho = Efrho(1000 * u.cm)
+        v = efrho * 2 * u.cm
+        assert v == 2000 * u.cm**2
+        assert not isinstance(v, Efrho)
+
+    def test_from_flam(self):
+        fluxd = 3.824064455850402e-15 * u.W / u.m**2 / u.um
+        wave = 10 * u.um
+        aper = 1 * u.arcsec
+        eph = dict(rh=1.5 * u.au, delta=1.0 * u.au)
+        efrho = Efrho.from_fluxd(wave, fluxd, aper, eph)
+        assert np.isclose(efrho.cm, 1000)
+
+    def test_from_fnu(self):
+        fluxd = 6.0961896974549115 * u.mJy
+        nu = 2.998e14 / 11.7 * u.Hz
+        aper = 1 * u.arcsec
+        eph = dict(rh=1.5 * u.au, delta=1.0 * u.au)
+        S = 1.711e14 * u.Jy
+        efrho = Efrho.from_fluxd(nu, fluxd, aper, eph)
+        assert np.isclose(efrho.cm, 33.0)
+
+    def test_fluxd(self):
+        efrho = Efrho(260.76955995377915, 'cm')
+        wave = 5 * u.um
+        aper = 1 * u.arcsec
+        eph = dict(rh=1.5 * u.au, delta=1.0 * u.au)
+        fluxd = efrho.fluxd(wave, aper, eph)
+        assert np.isclose(fluxd.value, 1e-16)
+    
