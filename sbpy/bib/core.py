e58d94544134b80e3c200ffb4f7273e6bf0d9304
Miguel de Val-Borro <miguel.deval@gmail.com>
2017-11-28 19:31:13 -0500

    to_text and to_bibtex implementation

--- sbpy/bib/core.py
+++ sbpy/bib/core.py
@@ -83,31 +83,61 @@ def track():
 def to_text():
     """convert bibcodes to human readable text
 
-    not yet implemented
+    Returns
+    -------
+    text : string
+      Text with human-readable information retrieved from ADS using the
+      bibcodes.
     """
+    import ads
+
     output = ''
     for task, ref in _bibliography.items():
         output += '{:s}:\n'.format(task)
         try:
             for key, val in ref.items():
-                # transform val to ads(val)
-                output += '  {:s}: {:s}\n'.format(key, val)
+                # request needed fields to avoid lazy loading
+                paper = list(ads.SearchQuery(bibcode=val, fl=['first_author',
+                                                              'author',
+                                                              'year']))[0]
+                if len(paper.author) > 4:
+                    author = '{:s} et al.'.format(paper.first_author.split(',')[0])
+                elif len(paper.author) > 1:
+                    author = ','.join([au.split(',')[0] for au in
+                                       paper.author[:-1]])
+                    author += ' & {:s}'.format(paper.author[-1].split(',')[0])
+                else:
+                    # single author
+                    author = paper.first_author.split(',')[0]
+
+                output += '  {:s}: {:s} {:s}, {:s}\n'.format(key, author,
+                                                             paper.year, val)
         except:
             pass
 
     return output
 
 def to_bibtex():
-    """ convert bibcodes to LaTeX bibtex
+    """ convert bibcodes to LaTeX BibTeX
 
-        not yet implemented
+    Returns
+    -------
+    text : string
+      ADS BibTex entries for all the bibliographic items.  Uses a query to the
+      export service to get the BibTeX for each reference.
     """
+    import ads
+
     output = ''
     for task, ref in _bibliography.items():
         try:
             for key, val in ref.items():
-                # transform val to ads.bibtex(val)
-                output += '% {:s}/{:s}:\n{:s}\n'.format(task, key, val)
+                # This method to get bibtex records avoids using multiple calls
+                # to the API that may impact rate limits
+                # https://github.com/adsabs/adsabs-dev-api/blob/master/export.md
+                query = ads.ExportQuery(val, format='bibtex')
+                bibtex = query.execute()
+                output += '% {:s}/{:s}:\n{:s}\n'.format(task, key, bibtex)
         except:
             pass
 
