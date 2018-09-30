7b653aec371a6a067d166ee1be269d8b273810fa
Michael S. P. Kelley <msk@astro.umd.edu>
2018-09-30 10:31:03 -0400

    import typo

--- sbpy/data/tests/test_names.py
+++ sbpy/data/tests/test_names.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import pytest
 
-from ..names import Names, TargetNameParseError, natrual_sort_key
+from ..names import Names, TargetNameParseError, natural_sort_key
 
 # name: expected result from parse_comet()
 comets = {
