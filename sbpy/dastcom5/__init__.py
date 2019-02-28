33f021f80eef6597686a5fc6bd1113632ac8baaf
Shreyas Bapat <bapat.shreyas@gmail.com>
2019-02-28 17:11:02 +0530

    Add tests for Dastcom5 module

--- sbpy/dastcom5/__init__.py
+++ sbpy/dastcom5/__init__.py
@@ -4,4 +4,4 @@ Functions related to NEOs and different NASA APIs.
 All of them are coded as part of SOCIS 2017 proposal for poliastro by Antonio Hidalgo.
 
 """
-from sbpy.dastcom5 import dastcom5
+from sbpy.dastcom5.dastcom5 import *
