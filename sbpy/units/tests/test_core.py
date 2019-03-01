e66666a112288d90c717d80e9f9765d5554cdc21
Michael S. P. Kelley <msk@astro.umd.edu>
2019-03-01 09:18:26 -0500

    Test for hundred_nm

--- sbpy/units/tests/test_core.py
+++ sbpy/units/tests/test_core.py
@@ -13,3 +13,8 @@ def test_predefined_string_roundtrip():
     assert u.Unit(VegaFluxDensity.to_string()) == VegaFluxDensity
     assert u.Unit(VegaMag.to_string()) == VegaMag
     assert u.Unit(hundred_nm.to_string()) == hundred_nm
+
+
+def test_hundred_nm():
+    c = (2 / hundred_nm).to('1/nm')
+    assert np.isclose(c.value, 0.02)
