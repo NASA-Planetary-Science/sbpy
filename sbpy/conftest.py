e61666ef820729015f13684494227e5f5ab34b27
Miguel de Val-Borro <miguel.deval@gmail.com>
2020-07-03 18:16:10 -0400

    conftest.py file updates

--- sbpy/conftest.py
+++ sbpy/conftest.py
@@ -2,6 +2,8 @@
 # by importing them here in conftest.py they are discoverable by py.test
 # no matter how it is invoked within the source tree.
 
+import os
+
 from astropy.version import version as astropy_version
 
 if astropy_version < '3.0':
@@ -19,28 +21,25 @@ else:
 ## exceptions
 # enable_deprecations_as_exceptions()
 
+
 # Uncomment and customize the following lines to add/remove entries from
 # the list of packages for which version numbers are displayed when running
 # the tests. Making it pass for KeyError is essential in some cases when
 # the package uses other astropy affiliated packages.
-try:
-    PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
+def pytest_configure(config):
+    config.option.astropy_header = True
+    PYTEST_HEADER_MODULES.pop('h5py')
     PYTEST_HEADER_MODULES['astroquery'] = 'astroquery'
-    del PYTEST_HEADER_MODULES['h5py']
-except KeyError:
-    pass
-
 # Uncomment the following lines to display the version number of the
 # package rather than the version number of Astropy in the top line when
 # running the tests.
-import os
 
 # This is to figure out the affiliated package version, rather than
 # using Astropy's
-try:
-    from .version import version
-except ImportError:
-    version = 'dev'
+    try:
+        from .version import version
+    except ImportError:
+        version = 'dev'
 
-packagename = os.path.basename(os.path.dirname(__file__))
-TESTED_VERSIONS[packagename] = version
+    packagename = os.path.basename(os.path.dirname(__file__))
+    TESTED_VERSIONS[packagename] = version
