1eac3dabc605ca4a0efbedcc82a7a9bbbeffe529
Michael S. P. Kelley <msk@astro.umd.edu>
2022-05-26 09:47:02 -0400

    Remove unused imports.

--- sbpy/spectroscopy/tests/test_spec_remote.py
+++ sbpy/spectroscopy/tests/test_spec_remote.py
@@ -1,15 +1,5 @@
 import os
 
-import numpy as np
-import astropy.units as u
-from astropy.time import Time
-from astropy.table import Table
-from astroquery.lamda import Lamda
-from astroquery.jplspec import JPLSpec
-from ...activity.gas import Haser, photo_timescale
-from ...data import Ephem
-from .. import Spectrum
-
 
 def data_path(filename):
     data_dir = os.path.join(os.path.dirname(__file__), 'data')
