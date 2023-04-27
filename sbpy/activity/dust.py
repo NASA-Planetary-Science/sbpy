ed533e592fb245dafc763bca509939a3020101bc
Michael S. P. Kelley <msk@astro.umd.edu>
2023-04-27 18:46:36 -0400

    Avoid tox -alldeps-oldestdeps
    alldeps was installing latest versions so oldestdeps were not being tested.

--- sbpy/activity/dust.py
+++ sbpy/activity/dust.py
@@ -8,11 +8,7 @@ All things dust coma related.
 
 """
 
-__all__ = [
-    'phase_HalleyMarcus',
-    'Afrho',
-    'Efrho'
-]
+__all__ = ["phase_HalleyMarcus", "Afrho", "Efrho"]
 
 
 from warnings import warn
@@ -21,6 +17,7 @@ import abc
 import numpy as np
 import astropy.units as u
 import astropy.version as astropy_version
+
 try:
     from astropy.utils.misc import InheritDocstrings
 except ImportError:
@@ -36,11 +33,13 @@ from ..spectroscopy.sources import SinglePointSpectrumError
 from .core import Aperture
 
 
-@bib.cite({
-    'Halley-Marcus phase function': '2011AJ....141..177S',
-    'Halley phase function': '1998Icar..132..397S',
-    'Marcus phase function': '2007ICQ....29...39M'
-})
+@bib.cite(
+    {
+        "Halley-Marcus phase function": "2011AJ....141..177S",
+        "Halley phase function": "1998Icar..132..397S",
+        "Marcus phase function": "2007ICQ....29...39M",
+    }
+)
 def phase_HalleyMarcus(phase):
     """Halley-Marcus composite dust phase function.
 
@@ -104,60 +103,201 @@ def phase_HalleyMarcus(phase):
 
     th = np.arange(181)
     ph = np.array(
-        [1.0000e+00, 9.5960e-01, 9.2170e-01, 8.8590e-01,
-         8.5220e-01, 8.2050e-01, 7.9060e-01, 7.6240e-01,
-         7.3580e-01, 7.1070e-01, 6.8710e-01, 6.6470e-01,
-         6.4360e-01, 6.2370e-01, 6.0490e-01, 5.8720e-01,
-         5.7040e-01, 5.5460e-01, 5.3960e-01, 5.2550e-01,
-         5.1220e-01, 4.9960e-01, 4.8770e-01, 4.7650e-01,
-         4.6590e-01, 4.5590e-01, 4.4650e-01, 4.3770e-01,
-         4.2930e-01, 4.2150e-01, 4.1420e-01, 4.0730e-01,
-         4.0090e-01, 3.9490e-01, 3.8930e-01, 3.8400e-01,
-         3.7920e-01, 3.7470e-01, 3.7060e-01, 3.6680e-01,
-         3.6340e-01, 3.6030e-01, 3.5750e-01, 3.5400e-01,
-         3.5090e-01, 3.4820e-01, 3.4580e-01, 3.4380e-01,
-         3.4210e-01, 3.4070e-01, 3.3970e-01, 3.3890e-01,
-         3.3850e-01, 3.3830e-01, 3.3850e-01, 3.3890e-01,
-         3.3960e-01, 3.4050e-01, 3.4180e-01, 3.4320e-01,
-         3.4500e-01, 3.4700e-01, 3.4930e-01, 3.5180e-01,
-         3.5460e-01, 3.5760e-01, 3.6090e-01, 3.6450e-01,
-         3.6830e-01, 3.7240e-01, 3.7680e-01, 3.8150e-01,
-         3.8650e-01, 3.9170e-01, 3.9730e-01, 4.0320e-01,
-         4.0940e-01, 4.1590e-01, 4.2280e-01, 4.3000e-01,
-         4.3760e-01, 4.4560e-01, 4.5400e-01, 4.6270e-01,
-         4.7200e-01, 4.8160e-01, 4.9180e-01, 5.0240e-01,
-         5.1360e-01, 5.2530e-01, 5.3750e-01, 5.5040e-01,
-         5.6380e-01, 5.7800e-01, 5.9280e-01, 6.0840e-01,
-         6.2470e-01, 6.4190e-01, 6.5990e-01, 6.7880e-01,
-         6.9870e-01, 7.1960e-01, 7.4160e-01, 7.6480e-01,
-         7.8920e-01, 8.1490e-01, 8.4200e-01, 8.7060e-01,
-         9.0080e-01, 9.3270e-01, 9.6640e-01, 1.0021e+00,
-         1.0399e+00, 1.0799e+00, 1.1223e+00, 1.1673e+00,
-         1.2151e+00, 1.2659e+00, 1.3200e+00, 1.3776e+00,
-         1.4389e+00, 1.5045e+00, 1.5744e+00, 1.6493e+00,
-         1.7294e+00, 1.8153e+00, 1.9075e+00, 2.0066e+00,
-         2.1132e+00, 2.2281e+00, 2.3521e+00, 2.4861e+00,
-         2.6312e+00, 2.7884e+00, 2.9592e+00, 3.1450e+00,
-         3.3474e+00, 3.5685e+00, 3.8104e+00, 4.0755e+00,
-         4.3669e+00, 4.6877e+00, 5.0418e+00, 5.4336e+00,
-         5.8682e+00, 6.3518e+00, 6.8912e+00, 7.4948e+00,
-         8.1724e+00, 8.9355e+00, 9.7981e+00, 1.0777e+01,
-         1.1891e+01, 1.3166e+01, 1.4631e+01, 1.6322e+01,
-         1.8283e+01, 2.0570e+01, 2.3252e+01, 2.6418e+01,
-         3.0177e+01, 3.4672e+01, 4.0086e+01, 4.6659e+01,
-         5.4704e+01, 6.4637e+01, 7.7015e+01, 9.2587e+01,
-         1.1237e+02, 1.3775e+02, 1.7060e+02, 2.1348e+02,
-         2.6973e+02, 3.4359e+02, 4.3989e+02, 5.6292e+02,
-         7.1363e+02, 8.8448e+02, 1.0533e+03, 1.1822e+03,
-         1.2312e+03])
+        [
+            1.0000e00,
+            9.5960e-01,
+            9.2170e-01,
+            8.8590e-01,
+            8.5220e-01,
+            8.2050e-01,
+            7.9060e-01,
+            7.6240e-01,
+            7.3580e-01,
+            7.1070e-01,
+            6.8710e-01,
+            6.6470e-01,
+            6.4360e-01,
+            6.2370e-01,
+            6.0490e-01,
+            5.8720e-01,
+            5.7040e-01,
+            5.5460e-01,
+            5.3960e-01,
+            5.2550e-01,
+            5.1220e-01,
+            4.9960e-01,
+            4.8770e-01,
+            4.7650e-01,
+            4.6590e-01,
+            4.5590e-01,
+            4.4650e-01,
+            4.3770e-01,
+            4.2930e-01,
+            4.2150e-01,
+            4.1420e-01,
+            4.0730e-01,
+            4.0090e-01,
+            3.9490e-01,
+            3.8930e-01,
+            3.8400e-01,
+            3.7920e-01,
+            3.7470e-01,
+            3.7060e-01,
+            3.6680e-01,
+            3.6340e-01,
+            3.6030e-01,
+            3.5750e-01,
+            3.5400e-01,
+            3.5090e-01,
+            3.4820e-01,
+            3.4580e-01,
+            3.4380e-01,
+            3.4210e-01,
+            3.4070e-01,
+            3.3970e-01,
+            3.3890e-01,
+            3.3850e-01,
+            3.3830e-01,
+            3.3850e-01,
+            3.3890e-01,
+            3.3960e-01,
+            3.4050e-01,
+            3.4180e-01,
+            3.4320e-01,
+            3.4500e-01,
+            3.4700e-01,
+            3.4930e-01,
+            3.5180e-01,
+            3.5460e-01,
+            3.5760e-01,
+            3.6090e-01,
+            3.6450e-01,
+            3.6830e-01,
+            3.7240e-01,
+            3.7680e-01,
+            3.8150e-01,
+            3.8650e-01,
+            3.9170e-01,
+            3.9730e-01,
+            4.0320e-01,
+            4.0940e-01,
+            4.1590e-01,
+            4.2280e-01,
+            4.3000e-01,
+            4.3760e-01,
+            4.4560e-01,
+            4.5400e-01,
+            4.6270e-01,
+            4.7200e-01,
+            4.8160e-01,
+            4.9180e-01,
+            5.0240e-01,
+            5.1360e-01,
+            5.2530e-01,
+            5.3750e-01,
+            5.5040e-01,
+            5.6380e-01,
+            5.7800e-01,
+            5.9280e-01,
+            6.0840e-01,
+            6.2470e-01,
+            6.4190e-01,
+            6.5990e-01,
+            6.7880e-01,
+            6.9870e-01,
+            7.1960e-01,
+            7.4160e-01,
+            7.6480e-01,
+            7.8920e-01,
+            8.1490e-01,
+            8.4200e-01,
+            8.7060e-01,
+            9.0080e-01,
+            9.3270e-01,
+            9.6640e-01,
+            1.0021e00,
+            1.0399e00,
+            1.0799e00,
+            1.1223e00,
+            1.1673e00,
+            1.2151e00,
+            1.2659e00,
+            1.3200e00,
+            1.3776e00,
+            1.4389e00,
+            1.5045e00,
+            1.5744e00,
+            1.6493e00,
+            1.7294e00,
+            1.8153e00,
+            1.9075e00,
+            2.0066e00,
+            2.1132e00,
+            2.2281e00,
+            2.3521e00,
+            2.4861e00,
+            2.6312e00,
+            2.7884e00,
+            2.9592e00,
+            3.1450e00,
+            3.3474e00,
+            3.5685e00,
+            3.8104e00,
+            4.0755e00,
+            4.3669e00,
+            4.6877e00,
+            5.0418e00,
+            5.4336e00,
+            5.8682e00,
+            6.3518e00,
+            6.8912e00,
+            7.4948e00,
+            8.1724e00,
+            8.9355e00,
+            9.7981e00,
+            1.0777e01,
+            1.1891e01,
+            1.3166e01,
+            1.4631e01,
+            1.6322e01,
+            1.8283e01,
+            2.0570e01,
+            2.3252e01,
+            2.6418e01,
+            3.0177e01,
+            3.4672e01,
+            4.0086e01,
+            4.6659e01,
+            5.4704e01,
+            6.4637e01,
+            7.7015e01,
+            9.2587e01,
+            1.1237e02,
+            1.3775e02,
+            1.7060e02,
+            2.1348e02,
+            2.6973e02,
+            3.4359e02,
+            4.3989e02,
+            5.6292e02,
+            7.1363e02,
+            8.8448e02,
+            1.0533e03,
+            1.1822e03,
+            1.2312e03,
+        ]
+    )
 
-    _phase = np.abs(u.Quantity(phase, 'deg').value)
+    _phase = np.abs(u.Quantity(phase, "deg").value)
 
     if scipy:
         Phi = splev(_phase, splrep(th, ph))
     else:
-        warn(sbe.OptionalPackageUnavailable(
-            'scipy is not present, using linear interpolation.'))
+        warn(
+            sbe.OptionalPackageUnavailable(
+                "scipy is not present, using linear interpolation."
+            )
+        )
         Phi = np.interp(_phase, th, ph)
 
     if np.iterable(_phase):
@@ -170,23 +310,23 @@ def phase_HalleyMarcus(phase):
 
 # checks if the installed astropy version is up-to-date
 if astropy_version.major < 4:
+
     class DustComaQuantityMeta(InheritDocstrings, abc.ABCMeta):
         pass
+
 else:
     # astropy >= 4.0
     DustComaQuantityMeta = abc.ABCMeta
 
 
-class DustComaQuantity(u.SpecificTypeQuantity,
-                       metaclass=DustComaQuantityMeta):
-    """Abstract base class for dust coma photometric models: Afrho, Efrho.
-    """
+class DustComaQuantity(u.SpecificTypeQuantity, metaclass=DustComaQuantityMeta):
+    """Abstract base class for dust coma photometric models: Afrho, Efrho."""
+
     _equivalent_unit = u.meter
     _include_easy_conversion_members = False
 
     def __new__(cls, value, unit=None, dtype=None, copy=True):
-        return super().__new__(cls, value, unit=unit, dtype=dtype,
-                               copy=copy)
+        return super().__new__(cls, value, unit=unit, dtype=dtype, copy=copy)
 
     @classmethod
     def from_fluxd(cls, wfb, fluxd, aper, eph, **kwargs):
@@ -216,8 +356,8 @@ class DustComaQuantity(u.SpecificTypeQuantity,
 
         """
 
-        fluxd1cm = cls(1 * u.cm).to_fluxd(wfb, aper, eph, unit=fluxd.unit,
-                                          **kwargs)
+        fluxd1cm = cls(1 * u.cm).to_fluxd(wfb, aper,
+                                          eph, unit=fluxd.unit, **kwargs)
 
         if isinstance(fluxd1cm, u.Magnitude):
             coma = cls((fluxd - fluxd1cm).physical * u.cm)
@@ -265,7 +405,7 @@ class DustComaQuantity(u.SpecificTypeQuantity,
         else:
             rho = aper
             ndim = np.ndim(rho)
-        rho = rho.to('km', sbu.projected_size(eph))
+        rho = rho.to("km", sbu.projected_size(eph))
 
         ndim = max(ndim, np.ndim(self))
 
@@ -284,7 +424,7 @@ class DustComaQuantity(u.SpecificTypeQuantity,
             _source = source.physical
         else:
             _source = source
-        fluxd = self * rho / eph['delta']**2 * _source
+        fluxd = self * rho / eph["delta"] ** 2 * _source
 
         # using Ephem can unnecessarily promote fluxd to an array
         if np.ndim(fluxd) > ndim:
@@ -352,8 +492,8 @@ class Afrho(DustComaQuantity):
     Examples
     --------
     >>> from sbpy.activity import Afrho
-    >>> print(Afrho(1000, 'cm'))
-    1000.0 cm
+    >>> print(Afrho(1000, 'cm'))  # doctest: +FLOAT_CMP
+    1000 cm
 
     """
 
@@ -361,7 +501,9 @@ class Afrho(DustComaQuantity):
     def from_fluxd(cls, wfb, fluxd, aper, eph, **kwargs):
         return super().from_fluxd(wfb, fluxd, aper, eph, **kwargs)
 
-    from_fluxd.__doc__ = DustComaQuantity.from_fluxd.__doc__ + """
+    from_fluxd.__doc__ = (
+        DustComaQuantity.from_fluxd.__doc__
+        + """
         Examples
         --------
         Convert observed flux density to Afρ, with a user-provided
@@ -380,13 +522,14 @@ class Afrho(DustComaQuantity):
         999.9999999999999 cm
 
         """
+    )
 
-    def to_fluxd(self, wfb, aper, eph, unit=None, phasecor=False,
-                 Phi=None):
-        return super().to_fluxd(wfb, aper, eph, unit=unit, phasecor=phasecor,
-                                Phi=Phi)
+    def to_fluxd(self, wfb, aper, eph, unit=None, phasecor=False, Phi=None):
+        return super().to_fluxd(wfb, aper, eph, unit=unit, phasecor=phasecor, Phi=Phi)
 
-    to_fluxd.__doc__ = DustComaQuantity.to_fluxd.__doc__ + """
+    to_fluxd.__doc__ = (
+        DustComaQuantity.to_fluxd.__doc__
+        + """
         phasecor: bool, optional
             Scale the result by the phase function ``Phi``, assuming
             ``Afrho`` is quoted for 0° phase.  Requires phase angle in
@@ -433,10 +576,10 @@ class Afrho(DustComaQuantity):
         15.321242371548918 mag(JM)
 
         """
+    )
 
-    @bib.cite({'model': '1984AJ.....89..579A'})
-    def _source_fluxd(self, wfb, eph, unit=None, phasecor=False,
-                      Phi=None, **kwargs):
+    @bib.cite({"model": "1984AJ.....89..579A"})
+    def _source_fluxd(self, wfb, eph, unit=None, phasecor=False, Phi=None, **kwargs):
         # get solar flux density
         sun = Sun.from_default()
         try:
@@ -444,22 +587,25 @@ class Afrho(DustComaQuantity):
         except SinglePointSpectrumError:
             S = sun(wfb, unit=unit)
 
-        if not (S.unit.is_equivalent(u.W / u.m**2 / u.um)
-                or S.unit.is_equivalent(u.W / u.m**2 / u.Hz)
-                or isinstance(S, u.Magnitude)):
+        if not (
+            S.unit.is_equivalent(u.W / u.m**2 / u.um)
+            or S.unit.is_equivalent(u.W / u.m**2 / u.Hz)
+            or isinstance(S, u.Magnitude)
+        ):
             raise ValueError(
-                'Solar flux density must have units of spectral flux '
-                'density, e.g., W/m2/μm or W/m2/Hz')
+                "Solar flux density must have units of spectral flux "
+                "density, e.g., W/m2/μm or W/m2/Hz"
+            )
 
         if phasecor:
             Phi = phase_HalleyMarcus if Phi is None else Phi
-            _Phi = Phi(eph['phase']) / Phi(0 * u.deg)
+            _Phi = Phi(eph["phase"]) / Phi(0 * u.deg)
         else:
             _Phi = 1
 
         # compute
         _S = S.physical if isinstance(S, u.Magnitude) else S
-        source = _S * _Phi / 4 * u.au**2 / eph['rh']**2
+        source = _S * _Phi / 4 * u.au**2 / eph["rh"] ** 2
 
         return source.to(S.unit)
 
@@ -543,8 +689,8 @@ class Efrho(DustComaQuantity):
     Examples
     --------
     >>> from sbpy.activity import Efrho
-    >>> print(Efrho(1000, 'cm'))
-    1000.0 cm
+    >>> print(Efrho(1000, 'cm'))  # pytest: +FLOAT_CMP
+    1000 cm
 
     """
 
@@ -552,7 +698,9 @@ class Efrho(DustComaQuantity):
     def from_fluxd(cls, wfb, fluxd, aper, eph, **kwargs):
         return super().from_fluxd(wfb, fluxd, aper, eph, **kwargs)
 
-    from_fluxd.__doc__ = DustComaQuantity.from_fluxd.__doc__ + """
+    from_fluxd.__doc__ = (
+        DustComaQuantity.from_fluxd.__doc__
+        + """
         Examples
         --------
         >>> from sbpy.activity import Efrho
@@ -566,13 +714,14 @@ class Efrho(DustComaQuantity):
         120.00836963059808 cm
 
     """
+    )
 
-    def to_fluxd(self, wfb, aper, eph, unit=None, Tscale=1.1,
-                 T=None, B=None):
-        return super().to_fluxd(wfb, aper, eph, unit=unit, Tscale=Tscale,
-                                T=T, B=B)
+    def to_fluxd(self, wfb, aper, eph, unit=None, Tscale=1.1, T=None, B=None):
+        return super().to_fluxd(wfb, aper, eph, unit=unit, Tscale=Tscale, T=T, B=B)
 
-    to_fluxd.__doc__ = DustComaQuantity.to_fluxd.__doc__ + """
+    to_fluxd.__doc__ = (
+        DustComaQuantity.to_fluxd.__doc__
+        + """
         Tscale : float, optional
             Scale factor for blackbody in LTE with sunlight.  Ignored
             if ``T`` or ``B`` is provided.
@@ -605,11 +754,12 @@ class Efrho(DustComaQuantity):
         0.006519545281786034 Jy
 
     """
+    )
 
-    @bib.cite({'model': '2013Icar..225..475K'})
+    @bib.cite({"model": "2013Icar..225..475K"})
     def _source_fluxd(self, wfb, eph, unit=None, Tscale=1.1, T=None, B=None):
         if T is None:
-            T = Tscale * 278 / np.sqrt(eph['rh'].to('au').value)
+            T = Tscale * 278 / np.sqrt(eph["rh"].to("au").value)
 
         if B is None:
             BB = BlackbodySource(T)
@@ -618,11 +768,14 @@ class Efrho(DustComaQuantity):
             except SinglePointSpectrumError:
                 B = BB(wfb, unit=unit)
         else:
-            if not (B.unit.is_equivalent(u.W / u.m**2 / u.um)
-                    or B.unit.is_equivalent(u.W / u.m**2 / u.Hz)
-                    or isinstance(B, u.Magnitude)):
+            if not (
+                B.unit.is_equivalent(u.W / u.m**2 / u.um)
+                or B.unit.is_equivalent(u.W / u.m**2 / u.Hz)
+                or isinstance(B, u.Magnitude)
+            ):
                 raise ValueError(
-                    'B must be a magnitude or have units of spectral '
-                    'flux density, e.g., W/m2/μm or W/m2/Hz')
+                    "B must be a magnitude or have units of spectral "
+                    "flux density, e.g., W/m2/μm or W/m2/Hz"
+                )
 
         return B
