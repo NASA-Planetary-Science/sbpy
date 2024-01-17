d42ccfb308ca4657de822acfd0914158be0e4960
Michael S. P. Kelley <msk@astro.umd.edu>
2024-01-16 19:47:48 -0500

    Finalize and test fitting Scattered and Thermal models.

--- sbpy/spectroscopy/tests/test_coma.py
+++ sbpy/spectroscopy/tests/test_coma.py
@@ -3,10 +3,8 @@
 import pytest
 import numpy as np
 import astropy.units as u
-from astropy.modeling.models import Scale
-import astropy.constants as const
-import synphot
-from ..sources import BlackbodySource, SpectralSource, SynphotRequired, Reddening
+from astropy.modeling import fitting
+from ..sources import BlackbodySource, SpectralSource, Reddening
 from ..coma import Scattered, Thermal
 from ..core import SpectralGradient
 from ...activity.dust import Afrho, Efrho
@@ -70,6 +68,31 @@ class TestScattered:
         b = Afrho.from_cross_section(cross_section, 1, rap, eph)
         assert u.isclose(a, b)
 
+    def test_fit(self):
+        wave = np.linspace(0.5, 0.6, 3) * u.um
+        S = SpectralGradient(2.0 * u.percent / hundred_nm, wave0=0.55 * u.um)
+        G = 100 * u.km**2
+        Ap = 0.05
+        rho = 5 * u.arcsec
+        eph = {
+            "rh": 1 * u.au,
+            "delta": 1 * u.au,
+            "phase": 60 * u.deg,
+        }
+        unit = u.Jy
+
+        afrho = Afrho.from_cross_section(G, Ap, rho, eph)
+        redden = Reddening(S)
+        spec = afrho.to_fluxd(wave, rho, eph, unit=unit) * redden(wave)
+        guess = Scattered(
+            eph, unit, cross_section=10 * u.km**2, S=3 * u.percent / hundred_nm
+        )
+        fitter = fitting.LevMarLSQFitter()
+        fit = fitter(guess, wave, spec)
+
+        assert u.isclose(fit.cross_section, Ap * G, rtol=1e-4)
+        assert u.isclose(fit.S, S, rtol=5e-3)
+
 
 class TestThermal:
     def test_init(self):
@@ -88,11 +111,8 @@ class TestThermal:
 
         # expected value
         B = BlackbodySource(278 / np.sqrt(eph["rh"].to_value("au")))
-        expected = (cross_section * B.observe(w, unit=unit) / eph["delta"] ** 2).to(
-            unit
-        )
+        expected = cross_section * B.observe(w, unit=unit) / np.pi / eph["delta"] ** 2
 
-        # test multiple wavelengths and no reddening
         t = Thermal(eph, unit, cross_section=cross_section, Tscale=Tscale)
         assert u.allclose(t(w), expected)
 
@@ -102,7 +122,7 @@ class TestThermal:
         eph = {"rh": 0.8 * u.au, "delta": 1 * u.au}
         t = Thermal(eph, unit, cross_section=cross_section, Tscale=Tscale)
         B = BlackbodySource(Tscale * 278 / np.sqrt(eph["rh"].to_value("au")))
-        expected = (cross_section * B(w, unit=unit) / eph["delta"] ** 2).to(unit)
+        expected = cross_section * B(w, unit=unit) / np.pi / eph["delta"] ** 2
         assert u.allclose(t(w), expected)
 
     def test_efrho(self):
@@ -111,9 +131,32 @@ class TestThermal:
         w = 20.0 * u.um
         eph = {"rh": 1 * u.au, "delta": 1 * u.au}
         rap = 1 * u.arcsec
+        epsilon = 0.95
         unit = u.Unit("W/(m2 um)")
 
         t = Thermal(eph, unit, cross_section=cross_section, Tscale=Tscale)
-        a = t.efrho(w, rap)
-        b = Efrho.from_cross_section(cross_section, 1, rap, eph)
+        a = t.efrho(epsilon, rap)
+        b = Efrho.from_cross_section(cross_section, epsilon, rap, eph)
         assert u.isclose(a, b)
+
+    def test_fit(self):
+        wave = np.linspace(8, 12, 3) * u.um
+        Tscale = 1.1
+        G = 100 * u.km**2
+        epsilon = 0.95
+        rho = 5 * u.arcsec
+        eph = {
+            "rh": 1 * u.au,
+            "delta": 1 * u.au,
+            "phase": 60 * u.deg,
+        }
+        unit = u.Jy
+
+        efrho = Efrho.from_cross_section(G, epsilon, rho, eph)
+        spec = efrho.to_fluxd(wave, rho, eph, unit=unit, Tscale=Tscale)
+        guess = Thermal(eph, unit, cross_section=100 * u.km**2, Tscale=1.08)
+        fitter = fitting.LevMarLSQFitter()
+        fit = fitter(guess, wave, spec)
+
+        assert u.isclose(fit.cross_section, epsilon * G)
+        assert u.isclose(fit.Tscale, Tscale)
