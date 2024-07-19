cf9b028a103ae5b80004782f648b5c05196e5ffb
Michael S. P. Kelley <msk@astro.umd.edu>
2024-07-19 11:10:54 -0400

    Use OORB_DATA from the user's/runner's environment; cache data in workflows

---
+++ .github/scripts/download-oorb-data.py
@@ -0,0 +1,30 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+sbpy test environment helper
+"""
+
+import os
+import shutil
+import urllib.request
+
+path = os.environ["OORB_DATA"]
+
+url = "https://github.com/mkelley/pyoorb-experiment/raw/sbpy-testing/pyoorb/data"
+
+files = [
+    "ET-UT.dat",
+    "TAI-UTC.dat",
+    "de430.dat",
+    "OBSCODE.dat",
+]
+
+# download files as needed
+for file_name in files:
+    fn = os.path.join(path, file_name)
+
+    if os.path.exists(fn):
+        continue
+
+    with urllib.request.urlopen(url) as response:
+        with open(fn, "wb") as outf:
+            shutil.copyfileobj(response, outf)
