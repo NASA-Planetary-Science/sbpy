0a9799c9b95fca40a29df4a0ce1a2248b93abdd0
Michael S. P. Kelley <msk@astro.umd.edu>
2024-07-19 11:38:47 -0400

    Fix download url

--- .github/scripts/download-oorb-data.py
+++ .github/scripts/download-oorb-data.py
@@ -9,7 +9,7 @@ import urllib.request
 
 path = os.environ["OORB_DATA"]
 
-url = "https://github.com/mkelley/pyoorb-experiment/raw/sbpy-testing/pyoorb/data"
+base_url = "https://github.com/mkelley/pyoorb-experiment/raw/sbpy-testing/pyoorb/data"
 
 files = [
     "ET-UT.dat",
@@ -21,10 +21,10 @@ files = [
 # download files as needed
 for file_name in files:
     fn = os.path.join(path, file_name)
-
     if os.path.exists(fn):
         continue
 
+    url = "/".join(base_url, file_name)
     with urllib.request.urlopen(url) as response:
         with open(fn, "wb") as outf:
             shutil.copyfileobj(response, outf)
