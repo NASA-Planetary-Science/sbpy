6b15b69286ea0e111cfb16b4f46e8810a57ef693
Michael S. P. Kelley <msk@astro.umd.edu>
2024-11-12 12:38:31 -0500

    surface_brightness only available for astropy >= 6.1

--- sbpy/units/typing.py
+++ sbpy/units/typing.py
@@ -2,13 +2,27 @@
 """sbpy unit and quantity typing."""
 
 from typing import Union
+from packaging.version import Version
+
 import astropy.units as u
+from astropy import __version__ as astropy_version
 
 UnitLike = Union[str, u.Unit]
-SpectralQuantity = Union[u.Quantity["length"], u.Quantity["frequency"]]
+SpectralQuantity = Union[
+    u.Quantity[u.physical.length], u.Quantity[u.physical.frequency]
+]
 SpectralFluxDensityQuantity = Union[
-    u.Quantity["spectral_flux_density"], u.Quantity["spectral_flux_density_wav"]
+    u.Quantity[u.physical.spectral_flux_density],
+    u.Quantity[u.physical.spectral_flux_density_wav],
 ]
-SpectralRadianceQuantity = Union[
-    u.Quantity["surface_brightness"], u.Quantity["surface_brightness_wav"]
-]
+
+if Version(astropy_version) < Version("6.1"):
+    SpectralRadianceQuantity = Union[
+        u.Quantity[u.physical.spectral_flux_density / u.sr],
+        u.Quantity[u.physical.spectral_flux_density_wav / u.sr],
+    ]
+else:
+    SpectralRadianceQuantity = Union[
+        u.Quantity[u.physical.surface_brightness],
+        u.Quantity[u.physical.surface_brightness_wav],
+    ]
