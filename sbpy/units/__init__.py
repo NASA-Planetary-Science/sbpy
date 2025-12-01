02c43346bca1e56f1c1f6eb14ef7aef86a4179f4
Michael S. P. Kelley <msk@astro.umd.edu>
2025-12-01 13:59:09 -0500

    Fix version comparison.

--- sbpy/units/__init__.py
+++ sbpy/units/__init__.py
@@ -12,7 +12,7 @@ from .core import *
 # DOCSTRING
 # This generates a docstring for this module that describes all of the
 # standard units defined here.
-if Version(astropy.__version__) <= Version("7.2.0"):
+if Version(astropy.__version__) < Version("7.2.0"):
     from astropy.units.utils import generate_unit_summary as _generate_unit_summary
 else:
     from astropy.units.docgen import generate_unit_summary as _generate_unit_summary
