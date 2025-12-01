1da739835d6952bafb60647f5578271a82f80de0
Michael S. P. Kelley <msk@astro.umd.edu>
2025-12-01 13:51:18 -0500

    Follow astropy's moved unit documnetation generator.

--- sbpy/units/__init__.py
+++ sbpy/units/__init__.py
@@ -1,17 +1,21 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-"""Common planetary astronomy units.
+"""Common planetary astronomy units."""
 
-"""
+import astropy
+from packaging.version import Version as Version
 
 from .core import *
 
 
 ###########################################################################
 # DOCSTRING
-
 # This generates a docstring for this module that describes all of the
 # standard units defined here.
-from astropy.units.utils import generate_unit_summary as _generate_unit_summary
+if Version(astropy.__version__) <= Version("7.2.0"):
+    from astropy.units.utils import generate_unit_summary as _generate_unit_summary
+else:
+    from astropy.units.docgen import generate_unit_summary as _generate_unit_summary
+
 if __doc__ is not None:
     __doc__ += _generate_unit_summary(globals())
